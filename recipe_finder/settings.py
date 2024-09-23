5d55f3a7ec370627795fd9b3ef928dd6075cea31
mumiak-mitch <akoth0mitch@gmail.com>
2024-09-23 15:34:28 +0300

    Added my render app url to allowed hosts

--- recipe_finder/settings.py
+++ recipe_finder/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'recipe-finder-3f5n.onrender.com',
+    'localhost',  # For local development
+    '127.0.0.1',   # For local development
+]
 
 
 # Application definition
