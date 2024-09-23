be89d7846ec42096521c0e2989342f99e8740df0
mumiak-mitch <akoth0mitch@gmail.com>
2024-09-23 15:30:08 +0300

    Getting ready for deployment

--- build.sh
+++ build.sh
@@ -2,11 +2,16 @@
 # exit on error
 set -o errexit
 
+# Install dependencies using Pipenv
 pipenv install --deploy --ignore-pipfile
 
+# Collect static files
 python manage.py collectstatic --no-input
-python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
+
+# Remove the migration command since no database is needed
+# python manage.py migrate  # This line is no longer needed
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
